c6a4a647d2c15566ff7e6208caf784ee481385a5
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-10-17 16:09:29 +0200

    Fix for lighter timestamp generating module.

--- bpfcap/rtl/timestamp.sv
+++ bpfcap/rtl/timestamp.sv
@@ -1,24 +1,30 @@
 // Freerun counter that returns number of nanoseconds elapsed since start of
 // the program.
 module timestamp
-                #(parameter FREQ = 50) // in MHz
+                #(parameter FREQ = 50) // in MHz // TODO: how to calculate increment without division?
                 (input logic clk,
                  input logic reset_n,
                  output logic[31:0] seconds,
                  output logic[31:0] nanoseconds);
 
-    logic [63:0] counter;
-    logic[6:0] increment;
+    logic [31:0] ctr_nanoseconds;
+    logic [31:0] ctr_seconds;
 
-    assign increment = (1 * 1000 ) / FREQ;
-    assign seconds = counter / 1_000_000_000;
-    assign nanoseconds = counter % 1_000_000_000;
+    assign seconds = ctr_seconds;
+    assign nanoseconds = ctr_nanoseconds;
 
     always_ff @(posedge clk) begin
         if (!reset_n) begin
-            counter <= '0;
+            ctr_nanoseconds <= '0;
+            ctr_seconds <= '0;
         end else begin
-            counter <= counter + increment;
+            if (ctr_nanoseconds === 'd1_000 - 'd20) begin//_000_000) begin
+                ctr_seconds <= ctr_seconds + 'b1;
+                ctr_nanoseconds <= '0;
+            end
+            else begin
+                ctr_nanoseconds <= ctr_nanoseconds + 'd20; // TODO: hardcoded 50MHz clock
+            end
         end
     end
 endmodule: timestamp
